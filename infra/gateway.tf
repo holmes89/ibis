4e8ffd01a148ade526d2720ff2b0c0a94c9d6aa1
Joel Holmes <holmes89@gmail.com>
2022-03-25 15:45:09 -0400

    update

--- infra/gateway.tf
+++ infra/gateway.tf
@@ -15,7 +15,7 @@ resource "aws_api_gateway_authorizer" "api_authorizer" {
   name          = "CognitoUserPoolAuthorizer"
   type          = "COGNITO_USER_POOLS"
   rest_api_id   = aws_api_gateway_rest_api.rest_api.id
-  provider_arns = [aws_cognito_user_pool.user_pool.id]
+  provider_arns = [aws_cognito_user_pool.user_pool.arn]
 }
 
 resource "aws_api_gateway_resource" "proxy" {
