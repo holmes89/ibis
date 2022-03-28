ef64848b240713b1b3cdcd18a34b50b077cfd2f5
Joel Holmes <holmes89@gmail.com>
2022-03-28 15:35:03 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from "react";
 
-function App() {
+import Amplify, { Auth, Hub } from 'aws-amplify';
+import Login from './components/Login';
+
+Amplify.configure({
+  Auth: {
+    userPoolId: 'us-east-2_Ekcud6vxr', //UserPool ID
+    region: 'us-east-2  ',
+    userPoolWebClientId: '2e21r8j61uje52ju1vsu0ae1uq' //WebClientId
+  }
+});
+
+const App = () => {
+  const [user, setUser] = useState(null);
+  
+  useEffect(() => {
+    Hub.listen("auth", ({ payload: { event, data } }) => {
+      debugger;
+      switch (event) {
+        case "signIn":
+        case "cognitoHostedUI":
+          getUser().then((userData) => setUser(userData));
+          break;
+        case "signOut":
+          setUser(null);
+          break;
+        case "signIn_failure":
+        case "cognitoHostedUI_failure":
+          console.log("Sign in failure", data);
+          break;
+      }
+    });
+
+    getUser().then((userData) => setUser(userData));
+  }, []);
+
+  const getUser = async () => {
+    return Auth.currentAuthenticatedUser()
+      .then((userData) => userData)
+      .catch(() => console.log("Not signed in"));
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {user ? (
+        <h2>Logged In</h2>
+      ) : (
+       <Login />
+      )}
     </div>
   );
 }
---
+++ src/components/Login/index.jsx
@@ -0,0 +1,57 @@
+import "./style.css";
+import React, { useState } from "react";
+import { Auth } from "aws-amplify";
+
+const Login = () => {
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  
+  
+  const login = () => {
+    debugger;
+    Auth.signIn(username, password).then((result) => {
+      console.log("logged in")
+     }).catch((err) => {
+      console.log(err)
+     })
+  }
+  return (
+    <section className="hero is-dark is-fullheight">
+  <div className="hero-body">
+    <div className="container">
+      <div className="columns is-centered">
+        <div className="column is-5-tablet is-4-desktop is-3-widescreen">
+          <div className="box">
+            <div className="field">
+              <label className="label">Email</label>
+              <div className="control has-icons-left">
+                <input type="email"  className="input"  onChange={(evt)=>setUsername(evt.target.value)}/>
+                <span className="icon is-small is-left">
+                  <i className="fa fa-envelope"></i>
+                </span>
+              </div>
+            </div>
+            <div className="field">
+              <label className="label">Password</label>
+              <div className="control has-icons-left">
+                <input type="password" placeholder="*******" className="input"  onChange={(evt)=>setPassword(evt.target.value)}/>
+                <span className="icon is-small is-left">
+                  <i className="fa fa-lock"></i>
+                </span>
+              </div>
+            </div>
+            <div className="field">
+              <button className="button is-success" onClick={login}>
+                Login
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+  );
+};
+
+export default Login;
